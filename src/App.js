a142f1b8e8a406394ee4c5912fff81b9618b7d7e
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-20 19:28:11 +0100

    WindowTracker challenge start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import WindowTracker from './WindowTracker';
 
 export default function App() {
+  /**
+  * Challenge:
+  * 1. Create state called `show`, default to `true`
+  * 2. When the button is clicked, toggle `show`
+  * 3. Only display `<WindowTracker>` if `show` is `true`
+  */
   return (
     <div className="container">
-      <h1>App component</h1>
+      <button>
+        Toggle WindowTracker
+      </button>
+      <WindowTracker />
     </div>
   );
 };
